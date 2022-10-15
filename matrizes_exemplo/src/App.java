b559a2fefc16af959c220430617516e38bc7027d
danielmjob <danielmarquesjob@gmail.com>
2022-10-15 15:31:06 -0300

    commit exemplo matrizes

--- matrizes_exemplo/src/App.java
+++ matrizes_exemplo/src/App.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*
+         * Construindo matrizes
+         */
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas LINHAS vai ter a matriz: ");
+        int linha = sc.nextInt();
+        System.out.print("Quantas COLUNAS vai ter a matriz: ");
+        int coluna = sc.nextInt();
+
+        int [][] mat = new int[linha][coluna];
+
+        for(int i =0; i < linha; i++){
+            for(int j = 0; j < coluna;j++){
+                System.out.print("Elemento ["+ i + ","+ j +"]: ");
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println();
+        System.out.println("Matriz digitada");
+        System.out.println();
+
+        for(int i = 0; i < linha; i++){
+
+            for(int j = 0; j < coluna; j++){
+                System.out.print(mat[i][j] + " ");
+            }
+            
+            System.out.println();
+            
+        }
+
+
+
+
+
+
+        sc.close();
+
+
+
     }
 }
