371d2052fb26d60399c3ff074d373805e3f83c13
danielmjob <danielmarquesjob@gmail.com>
2022-10-18 21:25:12 -0300

    execicio comprar dolar

---
+++ comprar_dolar/src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+import java.text.BreakIterator;
+import java.util.Locale;
+import java.util.Scanner;
+import util.CurrencyConverter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual o preço do Dolar? ");
+        double precoDolar = sc.nextDouble();
+
+        System.out.print("Quantos dolares você quer comprar? ");
+        double totalDolar = sc.nextDouble();
+
+        double result = CurrencyConverter.calcularDolar(totalDolar, precoDolar);
+        System.out.printf("Amount to be paid in reais =  %.2f%n", result);
+
+        sc.close();
+    }
+
+  
+}
---
+++ comprar_dolar/src/util/CurrencyConverter.java
@@ -0,0 +1,14 @@
+package util;
+
+public class CurrencyConverter {
+
+    public static final double IOF = 0.06;
+
+    public static double calcularDolar(double totalDolar, double precoDolar){
+        double valorFinalReal = (totalDolar * precoDolar);
+        return valorFinalReal += valorFinalReal*IOF;
+
+        //return totalDolar * precoDolar * (1.0 + IOF);
+    }
+    
+}
