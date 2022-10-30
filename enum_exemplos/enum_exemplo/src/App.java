cfee0ca42b56b96ddf50bc3873e4655671cb6bb0
danielmjob <danielmarquesjob@gmail.com>
2022-10-30 15:59:24 -0300

    cfinalizado o exemplo conversao enum para String

--- enum_exemplos/enum_exemplo/src/App.java
+++ enum_exemplos/enum_exemplo/src/App.java
@@ -1,5 +1,22 @@
+import java.time.LocalDateTime;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Order order = new Order(1080, LocalDateTime.now(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println();
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED; // Formato enum
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED"); // Formato String
+
+        System.out.println();
+        System.out.println(os1); // enum
+        System.out.println(os2); // String
+
     }
 }
