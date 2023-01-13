88953b99fa47a06a19e45bf53de40700b9aefc84
danielmjob <danielmarquesjob@gmail.com>
2023-01-13 08:33:57 -0300

    Criando arquivos com BufferedWriter e FileWriter

---
+++ ler-arquivos/file-exemplos/src/application/AppFour.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AppFour {
+
+    public static void main(String[] args) {
+        // Criando arquivos
+
+        String[] lines = new String[] { "Good morning", "Good afternoon", "Good nigth" };
+
+        String path = "c:\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) { 
+            // Sempre cria um novo arquivo. Para escrever dando continuidade acrescente o ,new assim new FileWriter(path,new)
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
