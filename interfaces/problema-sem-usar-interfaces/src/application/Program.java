2ac926f2b9ce1dd9003387fc59088230bb078e30
danielmjob <danielmarquesjob@gmail.com>
2023-01-17 16:23:19 -0300

    Resolvendo sem o uso de Interfaces

--- interfaces/problema-sem-usar-interfaces/src/application/Program.java
+++ interfaces/problema-sem-usar-interfaces/src/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
     public static void main(String[] args) {
@@ -27,32 +29,43 @@ public class Program {
          * exemplos
          */
 
+        /* Resolvendo sem usar INTERFACES */
 
+        System.out.println();
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-         /*Resolvendo sem usar INTERFACES */
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-         System.out.println();
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
 
-         Locale.setDefault(Locale.US);
-         Scanner sc = new Scanner(System.in);
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 
-         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
-         System.out.println("Entre com os dados do aluguel");
-         System.out.print("Modelo do carro: ");
-         String carModel = sc.nextLine();
+        System.out.print("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
 
-         System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
-         LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-         System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
-         LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
 
-         CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
 
-         sc.close();
+        rentalService.processInvoice(cr);
 
+        System.out.println();
+        System.out.println("FATURA: ");
+        System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment())) ;
+        System.out.println("Imposto: " + String.format("%.2f",  cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
 
+        sc.close();
 
     }
 }
