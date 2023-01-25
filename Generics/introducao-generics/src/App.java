17479bb4dfebf6091c228164e98ce11bc1760799
danielmjob <danielmarquesjob@gmail.com>
2023-01-25 10:01:48 -0300

    Resolução Usando Generics<>

--- Generics/introducao-generics/src/App.java
+++ Generics/introducao-generics/src/App.java
@@ -12,17 +12,32 @@ public class App {
          * exemplo. Em seguida, informar qual foi o primeiro valor informado.
          */
 
-         //Resolução para Inteiros e String (usando Object) >>>não funcional<<<
+        // Resolução Usando Generics<>
+            //Benefícios
+                // • Reuso
+                // • Type safety
+                // • Performance/
+
 
         System.out.println();
         Scanner sc = new Scanner(System.in);
 
-        PrintService ps = new PrintService();
+
+        // Para mudar o tipo basta alterar onde estiver Integer para String por exexmplo sem que haja erros no codigo
+
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        /*
+         * Type safety
+         * quando usada a parametrização nesse caso para Integer o programa não aceitará
+         * nenhum tipo que não seja inteiro 
+         */
 
         System.out.print("How many values? ");
         int num = sc.nextInt();
 
-        ps.addValue("Maria"); // Para teste misturando String com Integer
+       // essa linha deixa de funcionar ps.addValue("Maria");
 
         for (int i = 0; i < num; i++) {
             Integer value = sc.nextInt();
@@ -30,7 +45,7 @@ public class App {
         }
 
         ps.print();
-        Integer x = (Integer) ps.first();
+        Integer x = ps.first();
         System.out.println("First: " + x);
         sc.close();
     }
--- Generics/introducao-generics/src/services/PrintService.java
+++ Generics/introducao-generics/src/services/PrintService.java
@@ -3,16 +3,16 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
+public class PrintService<T> {  
+    // Esse T pode ser qualquer nome
+        //Lembrandar de usar o mesmo nome em toda a classe
+private List<T> list = new ArrayList<>();
 
-private List<Object> list = new ArrayList<>();
-
-
-public void addValue(Object value){
+public void addValue(T value){
     list.add(value);
 }
 
-public Object first(){
+public T first(){
     if (list.isEmpty()){
         throw new IllegalStateException("List is empty");
     }
