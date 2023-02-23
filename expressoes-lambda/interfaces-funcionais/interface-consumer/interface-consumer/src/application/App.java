5538090ce5370ebcfbd9c6277c53bde830359794
danielmjob <danielmarquesjob@gmail.com>
2023-02-23 09:52:10 -0300

    Expressão lambda inline

--- expressoes-lambda/interfaces-funcionais/interface-consumer/interface-consumer/src/application/App.java
+++ expressoes-lambda/interfaces-funcionais/interface-consumer/interface-consumer/src/application/App.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,13 +17,7 @@ public class App {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double factor = 1.1;
-
-        Consumer<Product> cons = p-> {
-            p.setPrice(p.getPrice() * factor);
-        };
-
-        list.forEach(cons);
+        list.forEach(p -> p.setPrice(p.getPrice()*1.1));      
         
         list.forEach(System.out::println);
 
