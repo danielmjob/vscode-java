70d7e2413ccb53ab320a6ad8123c00a2c9b87465
danielmjob <danielmarquesjob@gmail.com>
2023-03-01 14:15:28 -0300

    Transações - Correto - (Exception proposital)

--- jdbc/jdbc5-transacao-ACID/src/application/Program.java
+++ jdbc/jdbc5-transacao-ACID/src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
     public static void main(String[] args) {
@@ -17,26 +18,38 @@ public class Program {
 
             conn = DB.getConnection(); // conectando ao banco de dados
 
+            conn.setAutoCommit(false); // evita que as operações sejam confirmadas automaticamente
+
             st = conn.createStatement();
 
             int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
             int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
 
-            System.out.println("rows1 " + rows1);
 
             // Gerando um erro propositalmente durante o processo
-            // Dessa forma o programa vai quebrar porem terá executado uma parte e a outra não
+            // Dessa forma o programa vai quebrar porem mas devido ao uso de (setAutoCommit, commit e rollback) o promagam não executará nada
 
             int x = 1;
             if (x < 2){
                 throw new SQLException("FAKE ERROR");
             }
 
+            
+            System.out.println("rows1 " + rows1);
             System.out.println("rows2 " + rows2);
 
+            conn.commit(); // colocado no final para que so sejam feitas as alterações se tudo tiver dado certo
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            try {
+
+                conn.rollback(); // em caso de algum erro (exception) desfaça tudo ou seja volte para o modo original
+                throw new DbException("Trasaction rolled back! Caused by: " + e.getMessage());
+
+            } catch (SQLException e1) {
+                throw new DbException("Erro trying to rollback! Caused by:" + e1.getMessage());
+            } 
 
         } finally {
             DB.closeStatement(st);
