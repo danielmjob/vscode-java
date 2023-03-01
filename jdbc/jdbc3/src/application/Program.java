e4e284072a46ffd1f8d3b2408e51d5436d8a1a6b
danielmjob <danielmarquesjob@gmail.com>
2023-03-01 10:09:29 -0300

    Inserção com retorno de Id (unica inserção)

--- jdbc/jdbc3/src/application/Program.java
+++ jdbc/jdbc3/src/application/Program.java
@@ -3,7 +3,9 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -25,7 +27,8 @@ public class Program {
                 "INSERT INTO seller"
                 + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
                 + "VALUES "
-                + "(?,?,?,?,?)"); // ? PARA CADA CAMPO QUE SERÁ PREENCHIDO DEPOIS
+                + "(?,?,?,?,?)",  // ? PARA CADA CAMPO QUE SERÁ PREENCHIDO DEPOIS
+                Statement.RETURN_GENERATED_KEYS); // retorna a chave
 
                 st.setString(1, "Carl Purple"); // numero da interrogação, dado
                 st.setString(2, "carl@gmail.com");
@@ -34,6 +37,19 @@ public class Program {
                 st.setInt(5, 4);
 
                 int rowsAffected = st.executeUpdate(); // resulta no numero de linhas alteradas
+
+                if (rowsAffected > 0){
+                    ResultSet rs = st.getGeneratedKeys(); // retorna um objeto do tipo ResulSet com o valor da chave alterada
+                    
+                    // nesse caso não seria obrigatório o while pois esta sendo alterado apenas um vendedor que so resultaria e uma chave, porem ja esta sendo implementado casos onde seja inserido mais de um
+                    while(rs.next()){
+                        int id = rs.getInt(1); // pega o valor da primeira coluna (1)
+                        System.out.println("Done! Id = " + id);
+                    }
+                }
+                else{
+                    System.out.println("No rows affected");
+                }
 
                 System.out.println("Done! Rows affected: " + rowsAffected); // mostra quantas linhas foram alteradas
 
